a12a8c5400a8555edaa807afe46c712963d71f82
Marcelo Vicente Pesenti Fachinelli <Marcelo.Fachinelli@wmg.com>
2017-12-01 16:35:13 -0200

    Start project

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,93 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Recipe {
+
+    //Special type to relational database.
+    //Not suported by oracle prior to 12.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookingTime;
+    private String source;
+    private String directions;
+    //Difficulty dificulty
+    @Lob
+    private Byte[] image;
+
+    //Cascade makes Recipe to own this.
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+    public Recipe() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookingTime() {
+        return cookingTime;
+    }
+
+    public void setCookingTime(Integer cookingTime) {
+        this.cookingTime = cookingTime;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
